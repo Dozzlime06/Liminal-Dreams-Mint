cb72288f36928d1cac8ec0df85095f043748083a
HyperLeverage <backpack.exhange@gmail.com>
2025-11-08 22:38:34 +0800

    Create _app.js

---
+++ src/pages/_app.js
@@ -0,0 +1,30 @@
+import { PrivyProvider } from '@privy-io/react-auth';
+import { base } from 'viem/chains';
+import '../styles/globals.css';
+
+function MyApp({ Component, pageProps }) {
+  return (
+    <PrivyProvider
+      appId={process.env.NEXT_PUBLIC_PRIVY_APP_ID || ''}
+      config={{
+        loginMethods: ['wallet', 'email', 'google', 'twitter'],
+        appearance: {
+          theme: 'dark',
+          accentColor: '#8b5cf6',
+          logo: 'https://auth.privy.io/logos/privy-logo.png',
+        },
+        embeddedWallets: {
+          createOnLogin: 'users-without-wallets',
+          noPromptOnSignature: false,
+        },
+        defaultChain: base,
+        supportedChains: [base],
+        walletConnectCloudProjectId: 'your-walletconnect-project-id',
+      }}
+    >
+      <Component {...pageProps} />
+    </PrivyProvider>
+  );
+}
+
+export default MyApp;
