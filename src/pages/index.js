ee22325ff8a647f8f4073844306949cd6d837dd3
HyperLeverage <backpack.exhange@gmail.com>
2025-11-08 23:19:19 +0800

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,21 +1,23 @@
-import dynamic from 'next/dynamic';
-import Head from 'next/head';
-
-const NFTMintingDApp = dynamic(
-  () => import('../components/NFTMintingDApp'),
-  { ssr: false }
-);
+// src/pages/index.js
+import { useWallets } from '@privy-io/react-auth';
+import NFTMintingDApp from '../components/NFTMintingDApp';
 
 export default function Home() {
-  return (
-    <>
-      <Head>
-        <title>NFT Minting DApp</title>
-        <meta name="description" content="Mint your NFT on Base Network" />
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-      <NFTMintingDApp />
-    </>
-  );
+  const { wallet, connectWallet } = useWallets();
+
+  if (!wallet) {
+    return (
+      <div className="flex flex-col items-center justify-center h-screen bg-black text-white">
+        <h1 className="text-2xl mb-4">Please connect your wallet first via Privy!</h1>
+        <button
+          onClick={connectWallet} // this will open embedded wallet UI
+          className="px-6 py-3 bg-purple-600 rounded-lg hover:bg-purple-700"
+        >
+          Connect Wallet
+        </button>
+      </div>
+    );
+  }
+
+  return <NFTMintingDApp wallet={wallet} />;
 }
